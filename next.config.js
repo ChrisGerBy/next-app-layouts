72739959ab20c2a4bc2e7a61ccd2108a603af006
Krystsina Gerko <kristsina@behaviourlab.com>
2023-12-12 16:20:47 +0100

    fix(base-path): fix basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    basePath: '/next-app-layouts',
+    basePath: '/',
 };
 
 module.exports = nextConfig;
